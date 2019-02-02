b7001f235bf88d2f01ff1c19b8cf60f5ac07101b
KMcKenna@avionica.com <KevinBaselinesw@users.noreply.github.com>
2019-02-02 17:03:53 -0500

    add some statistics extensions

--- src/NumpyDotNet/NumpyDotNet/Extensions.cs
+++ src/NumpyDotNet/NumpyDotNet/Extensions.cs
@@ -246,5 +246,71 @@ namespace NumpyDotNet
             return data;
         }
 
+        public static double Mean<T>(this ndarray a)
+        {
+            return a.ToList<T>().Mean();
+        }
+        public static double Mean<T>(this IList<T> values)
+        {
+            return values.Count == 0 ? 0 : values.Mean(0, values.Count);
+        }
+        public static double Mean<T>(this IList<T> values, int start, int end)
+        {
+            double s = 0;
+
+            for (int i = start; i < end; i++)
+            {
+                s += Convert.ToDouble(values[i]);
+            }
+
+            return s / (end - start);
+        }
+
+        public static double Variance<T>(this ndarray a)
+        {
+            return a.ToList<T>().Variance();
+        }
+        public static double Variance<T>(this IList<T> values)
+        {
+            return values.Variance(values.Mean(), 0, values.Count);
+        }
+        public static double Variance<T>(this IList<T> values, double mean)
+        {
+            return values.Variance(mean, 0, values.Count);
+        }
+        public static double Variance<T>(this IList<T> values, double mean, int start, int end)
+        {
+            double variance = 0;
+
+            for (int i = start; i < end; i++)
+            {
+                variance += Math.Pow((Convert.ToDouble(values[i]) - mean), 2);
+            }
+
+            int n = end - start;
+            if (start > 0) n -= 1;
+
+            return variance / (n);
+        }
+
+        public static double StandardDeviation<T>(this ndarray a)
+        {
+            return a.ToList<T>().StandardDeviation();
+        }
+        public static double StandardDeviation<T>(this IList<T> values)
+        {
+            return values.Count == 0 ? 0 : values.StandardDeviation(0, values.Count);
+        }
+        public static double StandardDeviation<T>(this IList<T> values, int start, int end)
+        {
+            double mean = values.Mean(start, end);
+            double variance = values.Variance(mean, start, end);
+
+            return Math.Sqrt(variance);
+        }
+
     }
+
+ 
+
 }
