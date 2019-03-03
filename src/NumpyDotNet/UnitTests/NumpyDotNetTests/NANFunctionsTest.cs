02c5b09ce4ca136e6e5674f55fb29b2070c35192
KMcKenna@avionica.com <KevinBaselinesw@users.noreply.github.com>
2019-03-03 15:56:17 -0500

    adding a couple more group placeholders

---
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/NANFunctionsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumpyDotNet;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NumpyLib;
+
+namespace NumpyDotNetTests
+{
+    [TestClass]
+    public class NANFunctionsTest : TestBaseClass
+    {
+        [Ignore]
+        [TestMethod]
+        public void Test_NANFunctions_Placeholder()
+        {
+        }
+    }
+}
---
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/StatisticsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumpyDotNet;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NumpyLib;
+
+
+namespace NumpyDotNetTests
+{
+    [TestClass]
+    public class StatisticsTests : TestBaseClass
+    {
+        [Ignore]
+        [TestMethod]
+        public void Test_StatisticsFunctions_Placeholder()
+        {
+        }
+    }
+}
