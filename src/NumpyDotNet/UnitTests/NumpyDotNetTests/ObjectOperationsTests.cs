759ca2406fc981938df7102a74b13c5d0a3df907
KMcKenna@avionica.com <KevinBaselinesw@users.noreply.github.com>
2019-03-03 16:11:01 -0500

    adding several more placeholders for future efforts

---
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/ObjectOperationsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumpyDotNet;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NumpyLib;
+
+namespace NumpyDotNetTests
+{
+    /// <summary>
+    /// functions that test numpy support for objects
+    /// </summary>
+    [TestClass]
+    public class ObjectOperationsTests : TestBaseClass
+    {
+        [Ignore]
+        [TestMethod]
+        public void Test_ObjectOperations_Placeholder()
+        {
+        }
+    }
+}
