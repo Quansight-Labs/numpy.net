59d4b6c5b5ea52ca30784cd355f253a8d4a0bbc7
KMcKenna@avionica.com <KevinBaselinesw@users.noreply.github.com>
2019-03-31 06:37:53 -0400

    adding placeholder for tests not planning on supporting

---
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/NOT_PLANNING_TODO_Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumpyDotNet;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NumpyLib;
+
+namespace NumpyDotNetTests
+{
+    [TestClass]
+    public class NOT_PLANNING_TODO_Tests : TestBaseClass
+    {
+        [Ignore]
+        [TestMethod]
+        public void xxx_Test_NOTPLANNINGTODO_Placeholder()
+        {
+            // look for functions with NOT_PLANNING_TODO
+        }
+
+
+    }
+}
