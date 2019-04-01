9e25ff82180840d1671805d44e049f0dec14e029
KMcKenna@avionica.com <KevinBaselinesw@users.noreply.github.com>
2019-04-01 05:44:32 -0400

    more np.nanmin unit tests

--- src/NumpyDotNet/UnitTests/NumpyDotNetTests/NANFunctionsTests.cs
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/NANFunctionsTests.cs
@@ -87,25 +87,82 @@ namespace NumpyDotNetTests
         {
             var a = np.array(new float[,] { { 1, 2 }, { 3, float.NaN } });
             var b = np.nanmin(a);
+            Assert.AreEqual(1.0f, b.GetItem(0));
             print(b);
 
 
             var c = np.nanmin(a, axis: 0);
+            AssertArray(c, new float[] { 1.0f, 2.0f });
             print(c);
 
             var d = np.nanmin(a, axis: 1);
+            AssertArray(d, new float[] { 1.0f, 3.0f });
             print(d);
 
             // When positive infinity and negative infinity are present:
 
             var e = np.nanmin(new float[] { 1, 2, float.NaN, float.PositiveInfinity });
+            Assert.AreEqual(1.0f, e.GetItem(0));
             print(e);
 
             var f = np.nanmin(new float[] { 1, 2, float.NaN, float.NegativeInfinity });
+            Assert.AreEqual(float.NegativeInfinity, f.GetItem(0));
             print(f);
 
             var g = np.amin(new float[] { 1, 2, -3, float.NegativeInfinity });
+            Assert.AreEqual(float.NegativeInfinity, g.GetItem(0));
             print(g);
+        }
+
+        [TestMethod]
+        public void test_nanmin_2()
+        {
+            var a = np.array(new double[,] { { 1, 2 }, { 3, double.NaN } });
+            var b = np.nanmin(a);
+            Assert.AreEqual(1.0, b.GetItem(0));
+            print(b);
+
+
+            var c = np.nanmin(a, axis: 0);
+            AssertArray(c, new double[] { 1.0, 2.0 });
+            print(c);
+
+            var d = np.nanmin(a, axis: 1);
+            AssertArray(d, new double[] { 1.0, 3.0 });
+            print(d);
+
+            // When positive infinity and negative infinity are present:
+
+            var e = np.nanmin(new double[] { 1, 2, double.NaN, double.PositiveInfinity });
+            Assert.AreEqual(1.0, e.GetItem(0));
+            print(e);
+
+            var f = np.nanmin(new double[] { 1, 2, double.NaN, double.NegativeInfinity });
+            Assert.AreEqual(double.NegativeInfinity, f.GetItem(0));
+            print(f);
+
+            var g = np.amin(new double[] { 1, 2, -3, double.NegativeInfinity });
+            Assert.AreEqual(double.NegativeInfinity, g.GetItem(0));
+            print(g);
+        }
+
+        [TestMethod]
+        public void test_nanmin_3()
+        {
+            var a = np.array(new long[,] { { 1, 2 }, { 3,-4 } });
+            var b = np.nanmin(a);
+            Assert.AreEqual((long)-4, b.GetItem(0));
+            print(b);
+
+
+            var c = np.nanmin(a, axis: 0);
+            AssertArray(c, new long[] { 1, -4 });
+            print(c);
+
+            var d = np.nanmin(a, axis: 1);
+            AssertArray(d, new long[] { 1, -4 });
+            print(d);
+
         }
 
         [Ignore] // need to implement Nanfunctions
--- src/PythonUnitTests/NANFunctionsTests.py
+++ src/PythonUnitTests/NANFunctionsTests.py
@@ -9,7 +9,29 @@ class NANFunctionsTests(unittest.TestCase):
 
         a = np.array([[1, 2], [3, np.nan]])
 
-        aa = np.isnan(a)
+        b = np.nanmin(a)
+        print(b)
+    
+        c = np.nanmin(a, axis=0)
+        print(c)
+
+        d = np.nanmin(a, axis=1)
+        print(d)
+
+        # When positive infinity and negative infinity are present:
+
+        e = np.nanmin([1, 2, np.nan, np.inf])
+        print(e)
+
+        f = np.nanmin([1, 2, np.nan, np.NINF])
+        print(f)
+
+        g = np.amin([1, 2, -3, np.NINF])
+        print(g)
+
+    def test_nanmin_2(self):
+
+        a = np.array([[1, 2], [3, np.nan]], dtype=np.float64)
 
         b = np.nanmin(a)
         print(b)
@@ -29,6 +51,46 @@ class NANFunctionsTests(unittest.TestCase):
         print(f)
 
         g = np.amin([1, 2, -3, np.NINF])
+        print(g)
+
+        
+    def test_nanmin_3(self):
+
+        a = np.array([[1, 2], [3, -4]], dtype=np.int64)
+
+        b = np.nanmin(a)
+        print(b)
+    
+        c = np.nanmin(a, axis=0)
+        print(c)
+
+        d = np.nanmin(a, axis=1)
+        print(d)
+
+     
+
+    def test_nanmax_1(self):
+
+        a = np.array([[1, 2], [3, np.nan]])
+
+        b = np.nanmax(a)
+        print(b)
+    
+        c = np.nanmax(a, axis=0)
+        print(c)
+
+        d = np.nanmax(a, axis=1)
+        print(d)
+
+        # When positive infinity and negative infinity are present:
+
+        e = np.nanmax([1, 2, np.nan, np.NINF])
+        print(e)
+
+        f = np.nanmax([1, 2, np.nan, np.inf])
+        print(f)
+
+        g = np.amax([1, 2, -3, np.inf])
         print(g)
 
 
