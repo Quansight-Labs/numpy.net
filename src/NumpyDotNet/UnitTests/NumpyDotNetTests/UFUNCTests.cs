457dd9f22e20dce55caee1d62238c6e7f7cefe8a
KMcKenna@avionica.com <KevinBaselinesw@users.noreply.github.com>
2019-11-30 13:17:46 -0500

    add UNFUNC SUBTRACT tests

--- src/NumpyDotNet/UnitTests/NumpyDotNetTests/UFUNCTests.cs
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/UFUNCTests.cs
@@ -12,6 +12,7 @@ namespace NumpyDotNetTests
     [TestClass]
     public class UFUNCTests : TestBaseClass
     {
+        #region UFUNC ADD tests
         [TestMethod]
         public void test_UFUNC_AddReduce_1()
         {
@@ -108,6 +109,86 @@ namespace NumpyDotNetTests
             print(b);
         }
 
-    
+        #endregion
+
+
+        #region UFUNC SUBTRACT tests
+        [TestMethod]
+        public void test_UFUNC_SubtractReduce_1()
+        {
+            var x = np.arange(8);
+
+            var a = np.ufunc.reduce(NpyArray_Ops.npy_op_subtract, x);
+            Assert.AreEqual(-28, a.GetItem(0));
+            print(a);
+
+            x = np.arange(8).reshape((2, 2, 2));
+            var b = np.ufunc.reduce(NpyArray_Ops.npy_op_subtract, x);
+            AssertArray(b, new int[,] { { -4, -4 }, { -4, -4 } });
+            print(b);
+
+            var c = np.ufunc.reduce(NpyArray_Ops.npy_op_subtract, x, 0);
+            AssertArray(c, new int[,] { { -4, -4 }, { -4, -4 } });
+            print(c);
+
+            var d = np.ufunc.reduce(NpyArray_Ops.npy_op_subtract, x, 1);
+            AssertArray(d, new int[,] { { -2, -2 }, { -2, -2 } });
+            print(d);
+
+            var e = np.ufunc.reduce(NpyArray_Ops.npy_op_subtract, x, 2);
+            AssertArray(e, new int[,] { { -1, -1 }, { -1, -1 } });
+            print(e);
+
+        }
+
+        [TestMethod]
+        public void test_UFUNC_SubtractReduceAt_1()
+        {
+            var a = np.ufunc.reduceat(NpyArray_Ops.npy_op_subtract, np.arange(8), new long[] { 0, 4, 1, 5, 2, 6, 3, 7 })["::2"] as ndarray;
+            AssertArray(a, new int[] { -6, -8, -10, -12 });
+            print(a);
+
+            double retstep = 0;
+            var x = np.linspace(0, 15, ref retstep, 16).reshape((4, 4));
+            var b = np.ufunc.reduceat(NpyArray_Ops.npy_op_subtract, x, new long[] { 0, 3, 1, 2, 0 });
+            AssertArray(b, new double[,] {{-12.0, -13.0, -14.0, -15.0},{12.0, 13.0, 14.0, 15.0}, {4.0, 5.0, 6.0, 7.0},
+                                          {8.0, 9.0, 10.0, 11.0}, {-24.0, -26.0, -28.0, -30.0}});
+            print(b);
+
+            var c = np.ufunc.reduceat(NpyArray_Ops.npy_op_multiply, x, new long[] { 0, 3 }, axis: 1);
+            AssertArray(c, new double[,] { { 0.0, 3.0 }, { 120.0, 7.0 }, { 720.0, 11.0 }, { 2184.0, 15.0 } });
+            print(c);
+        }
+
+        [TestMethod]
+        public void test_UFUNC_SubtractOuter_1()
+        {
+            var x = np.arange(4);
+
+            var a = np.ufunc.outer(NpyArray_Ops.npy_op_subtract, null, x, x);
+            AssertShape(a, 4, 4);
+            print(a.shape);
+            AssertArray(a, new int[,] { { 0, -1, -2, -3 }, { 1, 0, -1, -2 }, { 2, 1, 0, -1 }, { 3, 2, 1, 0 } });
+            print(a);
+
+            x = np.arange(6).reshape((3, 2));
+            var y = np.arange(6).reshape((2, 3));
+            var b = np.ufunc.outer(NpyArray_Ops.npy_op_subtract, null, x, y);
+            AssertShape(b, 3, 2, 2, 3);
+            print(b.shape);
+
+            var ExpectedDataB = new int[,,,]
+
+                {{{{0,  -1,  -2}, {-3,  -4,  -5}}, {{1,  0,  -1}, {-2, -3, -4}}},
+                 {{{2,  1,  0}, {-1,  -2,  -3}}, {{3,  2,  1}, { 0,  -1,  -2}}},
+                 {{{4,  3,  2}, {1,  0,  -1}}, {{5,  4,  3}, { 2,  1, 0}}}};
+
+            AssertArray(b, ExpectedDataB);
+
+            print(b);
+        }
+
+        #endregion
+
     }
 }
--- src/PythonUnitTests/UFUNCTests.py
+++ src/PythonUnitTests/UFUNCTests.py
@@ -68,6 +68,55 @@ class Test_UFUNCTests(unittest.TestCase):
         print(b.shape)
         print(b)
 
+    def test_UFUNC_SubtractReduce_1(self):
+
+        x = np.arange(8);
+
+        a = np.subtract.reduce(x)
+        print(a)
+
+        x = np.arange(8).reshape((2,2,2))
+        b = np.subtract.reduce(x)
+        print(b)
+
+        c = np.subtract.reduce(x, 0)
+        print(c)
+
+        d = np.subtract.reduce(x, 1)
+        print(d)
+
+        e = np.subtract.reduce(x, 2)
+        print(e)
+
+
+    def test_UFUNC_SubtractReduceAt_1(self):
+
+        a =np.subtract.reduceat(np.arange(8),[0,4, 1,5, 2,6, 3,7])[::2]
+        print(a)
+        print("**********")
+
+        x = np.linspace(0, 15, 16).reshape(4,4)
+        b = np.subtract.reduceat(x, [0, 3, 1, 2, 0])
+        print(b)
+        print("**********")
+
+        c = np.multiply.reduceat(x, [0, 3], axis = 1)
+        print(c)
+
+    def test_UFUNC_SubtractOuter_1(self):
+
+        x = np.arange(4);
+
+        a = np.subtract.outer(x, x)
+        print(a.shape)
+        print(a)
+
+        x = np.arange(6).reshape((3,2))
+        y = np.arange(6).reshape((2,3))
+        b = np.subtract.outer(x, y)
+        print(b.shape)
+        print(b)
+
  
 
 if __name__ == '__main__':
