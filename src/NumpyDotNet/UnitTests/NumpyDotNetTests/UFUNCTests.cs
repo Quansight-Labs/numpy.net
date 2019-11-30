c12cd676f13d32c266667075b55bd65f729a7748
KMcKenna@avionica.com <KevinBaselinesw@users.noreply.github.com>
2019-11-30 13:34:04 -0500

    adding UFUNC MULTIPLY tests

--- src/NumpyDotNet/UnitTests/NumpyDotNetTests/UFUNCTests.cs
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/UFUNCTests.cs
@@ -190,5 +190,83 @@ namespace NumpyDotNetTests
 
         #endregion
 
+        #region UFUNC MULTIPLY tests
+        [TestMethod]
+        public void test_UFUNC_MultiplyReduce_1()
+        {
+            var x = np.arange(8);
+
+            var a = np.ufunc.reduce(NpyArray_Ops.npy_op_multiply, x);
+            Assert.AreEqual(0, a.GetItem(0));
+            print(a);
+
+            x = np.arange(8).reshape((2, 2, 2));
+            var b = np.ufunc.reduce(NpyArray_Ops.npy_op_multiply, x);
+            AssertArray(b, new int[,] { { 0, 5 }, { 12, 21 } });
+            print(b);
+
+            var c = np.ufunc.reduce(NpyArray_Ops.npy_op_multiply, x, 0);
+            AssertArray(c, new int[,] { {0, 5 }, { 12, 21 } });
+            print(c);
+
+            var d = np.ufunc.reduce(NpyArray_Ops.npy_op_multiply, x, 1);
+            AssertArray(d, new int[,] { { 0, 3 }, { 24, 35 } });
+            print(d);
+
+            var e = np.ufunc.reduce(NpyArray_Ops.npy_op_multiply, x, 2);
+            AssertArray(e, new int[,] { { 0, 6 }, { 20, 42 } });
+            print(e);
+
+        }
+
+        [TestMethod]
+        public void test_UFUNC_MultiplyReduceAt_1()
+        {
+            var a = np.ufunc.reduceat(NpyArray_Ops.npy_op_multiply, np.arange(8), new long[] { 0, 4, 1, 5, 2, 6, 3, 7 })["::2"] as ndarray;
+            AssertArray(a, new int[] { 0, 24, 120, 360 });
+            print(a);
+
+            double retstep = 0;
+            var x = np.linspace(0, 15, ref retstep, 16).reshape((4, 4));
+            var b = np.ufunc.reduceat(NpyArray_Ops.npy_op_multiply, x, new long[] { 0, 3, 1, 2, 0 });
+            AssertArray(b, new double[,] {{0.0, 45.0, 120.0, 231.0},{12.0, 13.0, 14.0, 15.0}, {4.0, 5.0, 6.0, 7.0},
+                                          {8.0, 9.0, 10.0, 11.0}, {0.0, 585.0, 1680.0, 3465.0}});
+            print(b);
+
+            var c = np.ufunc.reduceat(NpyArray_Ops.npy_op_multiply, x, new long[] { 0, 3 }, axis: 1);
+            AssertArray(c, new double[,] { { 0.0, 3.0 }, { 120.0, 7.0 }, { 720.0, 11.0 }, { 2184.0, 15.0 } });
+            print(c);
+        }
+
+        [TestMethod]
+        public void test_UFUNC_MultiplyOuter_1()
+        {
+            var x = np.arange(4);
+
+            var a = np.ufunc.outer(NpyArray_Ops.npy_op_multiply, null, x, x);
+            AssertShape(a, 4, 4);
+            print(a.shape);
+            AssertArray(a, new int[,] { { 0, 0, 0, 0 }, { 0, 1, 2, 3 }, { 0, 2, 4, 6 }, { 0, 3, 6, 9 } });
+            print(a);
+
+            x = np.arange(6).reshape((3, 2));
+            var y = np.arange(6).reshape((2, 3));
+            var b = np.ufunc.outer(NpyArray_Ops.npy_op_multiply, null, x, y);
+            AssertShape(b, 3, 2, 2, 3);
+            print(b.shape);
+
+            var ExpectedDataB = new int[,,,]
+
+                {{{{0,  0,  0}, {0,  0,  0}}, {{0,  1,  2}, {3, 4, 5}}},
+                 {{{0,  2,  4}, {6,  8,  10}}, {{0,  3,  6}, { 9,  12,  15}}},
+                 {{{0,  4,  8}, {12, 16, 20}}, {{0,  5,  10}, { 15, 20, 25}}}};
+
+            AssertArray(b, ExpectedDataB);
+
+            print(b);
+        }
+
+        #endregion
+
     }
 }
--- src/PythonUnitTests/UFUNCTests.py
+++ src/PythonUnitTests/UFUNCTests.py
@@ -123,6 +123,60 @@ class Test_UFUNCTests(unittest.TestCase):
         print(b)
 
     #endregion
+
+
+    #region UFUNC MULTIPLY tests
+
+    def test_UFUNC_MultiplyReduce_1(self):
+
+        x = np.arange(8);
+
+        a = np.multiply.reduce(x)
+        print(a)
+
+        x = np.arange(8).reshape((2,2,2))
+        b = np.multiply.reduce(x)
+        print(b)
+
+        c = np.multiply.reduce(x, 0)
+        print(c)
+
+        d = np.multiply.reduce(x, 1)
+        print(d)
+
+        e = np.multiply.reduce(x, 2)
+        print(e)
+
+
+    def test_UFUNC_MultiplyReduceAt_1(self):
+
+        a =np.multiply.reduceat(np.arange(8),[0,4, 1,5, 2,6, 3,7])[::2]
+        print(a)
+        print("**********")
+
+        x = np.linspace(0, 15, 16).reshape(4,4)
+        b = np.multiply.reduceat(x, [0, 3, 1, 2, 0])
+        print(b)
+        print("**********")
+
+        c = np.multiply.reduceat(x, [0, 3], axis = 1)
+        print(c)
+
+    def test_UFUNC_MultiplyOuter_1(self):
+
+        x = np.arange(4);
+
+        a = np.multiply.outer(x, x)
+        print(a.shape)
+        print(a)
+
+        x = np.arange(6).reshape((3,2))
+        y = np.arange(6).reshape((2,3))
+        b = np.multiply.outer(x, y)
+        print(b.shape)
+        print(b)
+
+    #endregion
  
 
 if __name__ == '__main__':
