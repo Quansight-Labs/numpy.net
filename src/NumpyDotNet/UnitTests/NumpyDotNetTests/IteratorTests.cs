e356008255bae118e11d2d5d5d5373258fe7dc5a
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2019-12-22 13:52:17 -0500

    more DECIMAL unit tests

--- src/NumpyDotNet/UnitTests/NumpyDotNetTests/IteratorTests.cs
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/IteratorTests.cs
@@ -36,9 +36,56 @@ namespace NumpyDotNetTests
         }
 
         [TestMethod]
+        public void test_nditer_1_DECIMAL()
+        {
+            var a = np.arange(0.1, 6.1, dtype: np.Decimal).reshape((2, 3));
+            var b = np.array(new decimal[] { 7, 8, 9 });
+
+            foreach (var aa in new nditer(a))
+            {
+                print(aa);
+            }
+
+            foreach (var aa in new nditer((a, b)))
+            {
+                print(aa);
+            }
+
+            foreach (var aa in new nditer((a, b, a, b)))
+            {
+                print(aa);
+            }
+
+        }
+
+        [TestMethod]
         public void test_ndindex_1()
         {
             var a = np.arange(0, 6).reshape((2, 3));  // force numpy to be initialized
+
+            foreach (var aa in new ndindex((2, 3)))
+            {
+                print(aa);
+            }
+
+
+            foreach (var aa in new ndindex((2, 3, 2)))
+            {
+                print(aa);
+            }
+
+            foreach (var aa in new ndindex((3)))
+            {
+                print(aa);
+            }
+
+        }
+
+
+        [TestMethod]
+        public void test_ndindex_1_DECIMAL()
+        {
+            var a = np.arange(0.1, 6.1, dtype: np.Decimal).reshape((2, 3));  // force numpy to be initialized
 
             foreach (var aa in new ndindex((2, 3)))
             {
@@ -62,6 +109,18 @@ namespace NumpyDotNetTests
         public void test_ndenumerate_1()
         {
             var a = np.arange(0, 6).reshape((2, 3));
+
+            foreach (ValueTuple<long[], object> aa in new ndenumerate(a))
+            {
+                print(aa.Item1);
+                print(aa.Item2);
+            }
+        }
+
+        [TestMethod]
+        public void test_ndenumerate_1_DECIMAL()
+        {
+            var a = np.arange(0.1, 6.1, dtype: np.Decimal).reshape((2, 3));
 
             foreach (ValueTuple<long[], object> aa in new ndenumerate(a))
             {
