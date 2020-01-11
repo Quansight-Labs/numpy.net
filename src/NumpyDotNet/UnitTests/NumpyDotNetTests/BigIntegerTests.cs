f259411cf82c08e7d8888aaaefa50a051b893d85
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2020-01-11 13:04:44 -0500

    more BIGINT unit tests

--- src/NumpyDotNet/UnitTests/NumpyDotNetTests/BigIntegerTests.cs
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/BigIntegerTests.cs
@@ -5539,8 +5539,6 @@ namespace NumpyDotNetTests
 
         #endregion
 
-#if true //COMPLEX_NUMBERS_UNIT_TESTS_TODO
-
         #region from StatisticsTests
 
         [TestMethod]
@@ -6004,15 +6002,15 @@ namespace NumpyDotNetTests
         #region from TwoDimBaseTests
 
         [TestMethod]
-        public void test_diag_1_BIGINT_TODO()
+        public void test_diag_1_BIGINT()
         {
-            ndarray m = np.arange(9, dtype: np.Decimal);
+            ndarray m = np.arange(9, dtype: np.BigInt);
             var n = np.diag(m);
 
             print(m);
             print(n);
 
-            var ExpectedDataN = new decimal[,]
+            var ExpectedDataN = new BigInteger[,]
                 {{0, 0, 0, 0, 0, 0, 0, 0, 0},
                  {0, 1, 0, 0, 0, 0, 0, 0, 0},
                  {0, 0, 2, 0, 0, 0, 0, 0, 0},
@@ -6025,24 +6023,24 @@ namespace NumpyDotNetTests
 
             AssertArray(n, ExpectedDataN);
 
-            m = np.arange(9, dtype: np.Decimal).reshape(new shape(3, 3));
+            m = np.arange(9, dtype: np.BigInt).reshape(new shape(3, 3));
             n = np.diag(m);
 
             print(m);
             print(n);
-            AssertArray(n, new decimal[] { 0, 4, 8 });
+            AssertArray(n, new BigInteger[] { 0, 4, 8 });
         }
 
         [TestMethod]
-        public void test_diagflat_1_BIGINT_TODO()
+        public void test_diagflat_1_BIGINT()
         {
-            ndarray m = np.arange(1, 5, dtype: np.Decimal).reshape(new shape(2, 2));
+            ndarray m = np.arange(1, 5, dtype: np.BigInt).reshape(new shape(2, 2));
             var n = np.diagflat(m);
 
             print(m);
             print(n);
 
-            var ExpectedDataN = new decimal[,]
+            var ExpectedDataN = new BigInteger[,]
             {
              {1, 0, 0, 0},
              {0, 2, 0, 0},
@@ -6051,13 +6049,13 @@ namespace NumpyDotNetTests
             };
             AssertArray(n, ExpectedDataN);
 
-            m = np.arange(1, 3, dtype: np.Decimal);
+            m = np.arange(1, 3, dtype: np.BigInt);
             n = np.diagflat(m, 1);
 
             print(m);
             print(n);
 
-            ExpectedDataN = new decimal[,]
+            ExpectedDataN = new BigInteger[,]
             {
              {0, 1, 0},
              {0, 0, 2},
@@ -6066,13 +6064,13 @@ namespace NumpyDotNetTests
 
             AssertArray(n, ExpectedDataN);
 
-            m = np.arange(1, 3, dtype: np.Decimal);
+            m = np.arange(1, 3, dtype: np.BigInt);
             n = np.diagflat(m, -1);
 
             print(m);
             print(n);
 
-            ExpectedDataN = new decimal[,]
+            ExpectedDataN = new BigInteger[,]
             {
              {0, 0, 0},
              {1, 0, 0},
@@ -6084,36 +6082,36 @@ namespace NumpyDotNetTests
         }
 
         [TestMethod]
-        public void test_fliplr_1_BIGINT_TODO()
+        public void test_fliplr_1_BIGINT()
         {
-            ndarray m = np.arange(8, dtype: np.Decimal).reshape(new shape(2, 2, 2));
+            ndarray m = np.arange(8, dtype: np.BigInt).reshape(new shape(2, 2, 2));
             var n = np.fliplr(m);
 
             print(m);
             print(n);
 
-            AssertArray(n, new decimal[,,] { { { 2, 3 }, { 0, 1 } }, { { 6, 7 }, { 4, 5 } } });
+            AssertArray(n, new BigInteger[,,] { { { 2, 3 }, { 0, 1 } }, { { 6, 7 }, { 4, 5 } } });
         }
 
         [TestMethod]
-        public void test_flipud_1_BIGINT_TODO()
+        public void test_flipud_1_BIGINT()
         {
-            ndarray m = np.arange(8, dtype: np.Decimal).reshape(new shape(2, 2, 2));
+            ndarray m = np.arange(8, dtype: np.BigInt).reshape(new shape(2, 2, 2));
             var n = np.flipud(m);
 
             print(m);
             print(n);
 
-            AssertArray(n, new decimal[,,] { { { 4, 5 }, { 6, 7 } }, { { 0, 1 }, { 2, 3 } } });
+            AssertArray(n, new BigInteger[,,] { { { 4, 5 }, { 6, 7 } }, { { 0, 1 }, { 2, 3 } } });
         }
 
         [TestMethod]
-        public void test_tri_1_BIGINT_TODO()
+        public void test_tri_1_BIGINT()
         {
-            ndarray a = np.tri(3, 5, 2, dtype: np.Decimal);
+            ndarray a = np.tri(3, 5, 2, dtype: np.BigInt);
             print(a);
 
-            var ExpectedDataA = new decimal[,]
+            var ExpectedDataA = new BigInteger[,]
             {
              {1, 1, 1, 0, 0},
              {1, 1, 1, 1, 0},
@@ -6122,28 +6120,28 @@ namespace NumpyDotNetTests
             AssertArray(a, ExpectedDataA);
 
             print("***********");
-            ndarray b = np.tri(3, 5, -1, dtype: np.Decimal);
+            ndarray b = np.tri(3, 5, -1, dtype: np.BigInt);
             print(b);
 
-            var ExpectedDataB = new decimal[,]
+            var ExpectedDataB = new BigInteger[,]
             {
-             {0.0m, 0.0m, 0.0m, 0.0m, 0.0m},
-             {1.0m, 0.0m, 0.0m, 0.0m, 0.0m},
-             {1.0m, 1.0m, 0.0m, 0.0m, 0.0m}
+             {0, 0, 0, 0, 0},
+             {1, 0, 0, 0, 0},
+             {1, 1, 0, 0, 0}
             };
             AssertArray(b, ExpectedDataB);
         }
 
         [TestMethod]
-        public void test_tril_1_BIGINT_TODO()
+        public void test_tril_1_BIGINT()
         {
-            ndarray a = np.array(new decimal[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } });
+            ndarray a = np.array(new BigInteger[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } });
             ndarray b = np.tril(a, -1);
             print(a);
             print("***********");
             print(b);
 
-            var ExpectedDataB = new decimal[,]
+            var ExpectedDataB = new BigInteger[,]
             {
              {0, 0, 0},
              {4, 0, 0},
@@ -6155,15 +6153,15 @@ namespace NumpyDotNetTests
         }
 
         [TestMethod]
-        public void test_triu_1_BIGINT_TODO()
+        public void test_triu_1_BIGINT()
         {
-            ndarray a = np.array(new decimal[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } });
+            ndarray a = np.array(new BigInteger[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } });
             ndarray b = np.triu(a, -1);
             print(a);
             print("***********");
             print(b);
 
-            var ExpectedDataB = new decimal[,]
+            var ExpectedDataB = new BigInteger[,]
             {
              {1, 2, 3},
              {4, 5, 6},
@@ -6175,61 +6173,61 @@ namespace NumpyDotNetTests
         }
 
         [TestMethod]
-        public void test_vander_1_BIGINT_TODO()
+        public void test_vander_1_BIGINT()
         {
-            var x = np.array(new decimal[] { 1, 2, 3, 5 });
+            var x = np.array(new BigInteger[] { 1, 2, 3, 5 });
             int N = 3;
             var y = np.vander(x, N);
-            AssertArray(y, new decimal[,] { { 1, 1, 1 }, { 4, 2, 1 }, { 9, 3, 1 }, { 25, 5, 1 } });
+            AssertArray(y, new BigInteger[,] { { 1, 1, 1 }, { 4, 2, 1 }, { 9, 3, 1 }, { 25, 5, 1 } });
             print(y);
 
             y = np.vander(x);
-            AssertArray(y, new decimal[,] { { 1, 1, 1, 1 }, { 8, 4, 2, 1 }, { 27, 9, 3, 1 }, { 125, 25, 5, 1 } });
+            AssertArray(y, new BigInteger[,] { { 1, 1, 1, 1 }, { 8, 4, 2, 1 }, { 27, 9, 3, 1 }, { 125, 25, 5, 1 } });
             print(y);
 
             y = np.vander(x, increasing: true);
-            AssertArray(y, new decimal[,] { { 1, 1, 1, 1 }, { 1, 2, 4, 8 }, { 1, 3, 9, 27 }, { 1, 5, 25, 125 } });
+            AssertArray(y, new BigInteger[,] { { 1, 1, 1, 1 }, { 1, 2, 4, 8 }, { 1, 3, 9, 27 }, { 1, 5, 25, 125 } });
             print(y);
 
             return;
         }
 
         [TestMethod]
-        public void test_mask_indices_BIGINT_TODO()
+        public void test_mask_indices_BIGINT()
         {
             var iu = np.mask_indices(3, np.triu);
             AssertArray(iu[0], new Int64[] { 0, 0, 0, 1, 1, 2 });
             AssertArray(iu[1], new Int64[] { 0, 1, 2, 1, 2, 2 });
             print(iu);
 
-            var a = np.arange(9, dtype: np.Decimal).reshape((3, 3));
+            var a = np.arange(9, dtype: np.BigInt).reshape((3, 3));
             var b = a[iu] as ndarray;
-            AssertArray(b, new decimal[] { 0, 1, 2, 4, 5, 8 });
+            AssertArray(b, new BigInteger[] { 0, 1, 2, 4, 5, 8 });
             print(b);
 
             var iu1 = np.mask_indices(3, np.triu, 1);
 
             var c = a[iu1] as ndarray;
-            AssertArray(c, new decimal[] { 1, 2, 5 });
+            AssertArray(c, new BigInteger[] { 1, 2, 5 });
             print(c);
 
             return;
         }
 
         [TestMethod]
-        public void test_tril_indices_BIGINT_TODO()
+        public void test_tril_indices_BIGINT()
         {
             var il1 = np.tril_indices(4);
             var il2 = np.tril_indices(4, 2);
 
-            var a = np.arange(16, dtype: np.Decimal).reshape((4, 4));
+            var a = np.arange(16, dtype: np.BigInt).reshape((4, 4));
             var b = a[il1] as ndarray;
-            AssertArray(b, new decimal[] { 0, 4, 5, 8, 9, 10, 12, 13, 14, 15 });
+            AssertArray(b, new BigInteger[] { 0, 4, 5, 8, 9, 10, 12, 13, 14, 15 });
             print(b);
 
             a[il1] = -1;
 
-            var ExpectedDataA1 = new decimal[,]
+            var ExpectedDataA1 = new BigInteger[,]
                 {{-1,  1, 2,  3}, {-1, -1,  6,  7},
                  {-1, -1,-1, 11}, {-1, -1, -1, -1}};
             AssertArray(a, ExpectedDataA1);
@@ -6237,7 +6235,7 @@ namespace NumpyDotNetTests
 
             a[il2] = -10;
 
-            var ExpectedDataA2 = new decimal[,]
+            var ExpectedDataA2 = new BigInteger[,]
                 {{-10, -10, -10,  3}, {-10, -10, -10, -10},
                  {-10, -10,-10, -10}, {-10, -10, -10, -10}};
             AssertArray(a, ExpectedDataA2);
@@ -6247,9 +6245,9 @@ namespace NumpyDotNetTests
         }
 
         [TestMethod]
-        public void test_tril_indices_from_BIGINT_TODO()
+        public void test_tril_indices_from_BIGINT()
         {
-            var a = np.arange(16, dtype: np.Decimal).reshape((4, 4));
+            var a = np.arange(16, dtype: np.BigInt).reshape((4, 4));
             var il1 = np.tril_indices_from(a, 0);
 
             AssertArray(il1[0], new Int64[] { 0, 1, 1, 2, 2, 2, 3, 3, 3, 3 });
@@ -6266,19 +6264,19 @@ namespace NumpyDotNetTests
         }
 
         [TestMethod]
-        public void test_triu_indices_BIGINT_TODO()
+        public void test_triu_indices_BIGINT()
         {
             var il1 = np.triu_indices(4);
             var il2 = np.triu_indices(4, 2);
 
-            var a = np.arange(16, dtype: np.Decimal).reshape((4, 4));
+            var a = np.arange(16, dtype: np.BigInt).reshape((4, 4));
             var b = a[il1] as ndarray;
-            AssertArray(b, new decimal[] { 0, 1, 2, 3, 5, 6, 7, 10, 11, 15 });
+            AssertArray(b, new BigInteger[] { 0, 1, 2, 3, 5, 6, 7, 10, 11, 15 });
             print(b);
 
             a[il1] = -1;
 
-            var ExpectedDataA1 = new decimal[,]
+            var ExpectedDataA1 = new BigInteger[,]
                 {{-1, -1, -1, -1}, { 4, -1, -1, -1},
                  { 8,  9, -1, -1}, {12, 13, 14, -1}};
             AssertArray(a, ExpectedDataA1);
@@ -6286,7 +6284,7 @@ namespace NumpyDotNetTests
 
             a[il2] = -10;
 
-            var ExpectedDataA2 = new decimal[,]
+            var ExpectedDataA2 = new BigInteger[,]
                 {{-1, -1, -10, -10}, {4,  -1, -1, -10},
                  { 8,  9, -1,  -1},  {12, 13, 14, -1}};
             AssertArray(a, ExpectedDataA2);
@@ -6296,9 +6294,9 @@ namespace NumpyDotNetTests
         }
 
         [TestMethod]
-        public void test_triu_indices_from_BIGINT_TODO()
+        public void test_triu_indices_from_BIGINT()
         {
-            var a = np.arange(16, dtype: np.Decimal).reshape((4, 4));
+            var a = np.arange(16, dtype: np.BigInt).reshape((4, 4));
             var il1 = np.triu_indices_from(a, 0);
 
             AssertArray(il1[0], new Int64[] { 0, 0, 0, 0, 1, 1, 1, 2, 2, 3 });
@@ -6315,6 +6313,8 @@ namespace NumpyDotNetTests
         }
 
         #endregion
+
+#if true //COMPLEX_NUMBERS_UNIT_TESTS_TODO
 
         #region from ShapeBaseTests
 
