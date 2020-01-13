2f08ef86d5c8f8f7e033b3bb8f1e1bcfff00ff4d
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2020-01-13 18:20:33 -0500

    new sample project for BIGINT data types

---
+++ src/NumpyDotNet/SampleApps/Int64VsBigInteger/Program.cs
@@ -0,0 +1,61 @@
+﻿using NumpyDotNet;
+using NumpyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Int64VsBigInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int LoopCount = 10000;
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+            var matrix = np.arange(0, 1600, dtype: np.Int64).reshape(new shape(40, -1));
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                matrix = matrix / 3;
+                matrix = matrix * 3;
+            }
+
+            var output = matrix.A(new Slice(15, 25, 2), new Slice(15, 25, 2));
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("Int64 calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine(output);
+            Console.WriteLine("************\n");
+
+            sw.Reset();
+            sw.Start();
+
+            matrix = np.arange(0, 1600, dtype: np.BigInt).reshape(new shape(40, -1));
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                matrix = matrix / 3;
+                matrix = matrix * 3;
+            }
+
+            output = matrix.A(new Slice(15, 25, 2), new Slice(15, 25, 2));
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("BigInteger calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+
+            Console.WriteLine(output);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
