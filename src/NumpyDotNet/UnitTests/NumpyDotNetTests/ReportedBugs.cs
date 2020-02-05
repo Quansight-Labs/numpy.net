75c9db476e0b3feca870feab3dd5b80948a6c82c
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2020-02-05 06:43:13 -0500

    make sure it works for more than 2 dimensions

--- src/NumpyDotNet/UnitTests/NumpyDotNetTests/ReportedBugs.cs
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/ReportedBugs.cs
@@ -21,6 +21,16 @@ namespace NumpyDotNetTests
             print(y);
             Assert.AreEqual(3, (int)y[0, 3]);
             Assert.AreEqual(8, (int)y[1, 3]);
+
+            x = np.arange(20, dtype: np.Int32);
+            y = x.reshape(new shape(2, 2, 5));
+            print(y);
+            Assert.AreEqual(3, (int)y[0, 0, 3]);
+            Assert.AreEqual(8, (int)y[0, 1, 3]);
+
+            Assert.AreEqual(13, (int)y[1, 0, 3]);
+            Assert.AreEqual(18, (int)y[1, 1, 3]);
+
         }
     }
 }
