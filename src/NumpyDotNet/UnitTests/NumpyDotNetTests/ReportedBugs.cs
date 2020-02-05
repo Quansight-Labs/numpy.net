04dcf762596e17d4c18ab4f84597fc922db60187
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2020-02-05 05:58:27 -0500

    add unit test for user reported bugs

---
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/ReportedBugs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumpyDotNet;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NumpyLib;
+using System.Threading.Tasks;
+
+namespace NumpyDotNetTests
+{
+    [TestClass]
+    public class ReportedBugs : TestBaseClass
+    {
+        [TestMethod]
+        public void test_multi_index_selection_Jan52020()
+        {
+            var x = np.arange(10, dtype: np.Int32);
+            var y = x.reshape(new shape(2, 5));
+            print(y);
+            Assert.AreEqual(3, (int)y[0, 3]);
+            Assert.AreEqual(8, (int)y[1, 3]);
+        }
+    }
+}
