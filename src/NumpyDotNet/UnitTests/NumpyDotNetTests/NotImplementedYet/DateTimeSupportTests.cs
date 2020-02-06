49ce17fbe55d9121283948de00c9657783c74b69
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2020-02-06 08:17:53 -0500

    reminder to verify DateTime stuff

--- src/NumpyDotNet/UnitTests/NumpyDotNetTests/NotImplementedYet/DateTimeSupportTests.cs
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/NotImplementedYet/DateTimeSupportTests.cs
@@ -12,6 +12,13 @@ namespace NumpyDotNetTests
     [TestClass]
     public class DateTimeSupportTests : TestBaseClass
     {
+        [TestMethod]
+        public void xxx_Test_DateTimeSupport_Placeholder()
+        {
+            Assert.Fail("verify that DateTime/TimeDiff can be done via object arrays");
+        }
+
+
 #if NOT_PLANNING_TODO
         [Ignore]
         [TestMethod]
