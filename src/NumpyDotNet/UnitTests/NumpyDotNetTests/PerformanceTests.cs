1ed5b397aaedf92c62d669320e98f25c8131d0a9
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2020-02-20 05:38:01 -0500

    let's duplicate the performance tests for floats

--- src/NumpyDotNet/UnitTests/NumpyDotNetTests/PerformanceTests.cs
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/PerformanceTests.cs
@@ -13,9 +13,10 @@ namespace NumpyDotNetTests
     [TestClass]
     public class PerformanceTests : TestBaseClass
     {
+        #region DOUBLE tests
         [Ignore]
         [TestMethod]
-        public void Performance_ScalarOperation()
+        public void Performance_ScalarOperation_DOUBLE()
         {
             int LoopCount = 200;
 
@@ -36,14 +37,14 @@ namespace NumpyDotNetTests
 
             sw.Stop();
 
-            Console.WriteLine(string.Format("Int64 calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine(string.Format("DOUBLE calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
             Console.WriteLine(output.ToString());
             Console.WriteLine("************\n");
         }
-
+   
         [Ignore]
         [TestMethod]
-        public void Performance_ScalarOperation_NotContiguous()
+        public void Performance_ScalarOperation_NotContiguous_DOUBLE()
         {
             int LoopCount = 200;
 
@@ -66,14 +67,14 @@ namespace NumpyDotNetTests
 
             sw.Stop();
 
-            Console.WriteLine(string.Format("Int64 calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine(string.Format("DOUBLE calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
             Console.WriteLine(output.ToString());
             Console.WriteLine("************\n");
         }
 
         [Ignore]
         [TestMethod]
-        public void Performance_ScalarOperation_InLine()
+        public void Performance_ScalarOperation_InLine_DOUBLE()
         {
             int LoopCount = 200;
 
@@ -98,54 +99,15 @@ namespace NumpyDotNetTests
 
             sw.Stop();
 
-            Console.WriteLine(string.Format("Int64 calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine(string.Format("DOUBLE calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
             Console.WriteLine(output.ToString());
             Console.WriteLine("************\n");
         }
+   
 
         [Ignore]
         [TestMethod]
-        public void Performance_ScalarOperation_InLine2()
-        {
-            int LoopCount = 200;
-
-            // get the numeric ops background threads started
-            var x = np.arange(1);
-
-            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
-
-            sw.Start();
-
-            var matrix = np.arange(1600000).astype(np.Int64).reshape((40, -1));
-
-            var src = matrix.Array.data.datap as Int64[];
-            int[] divisor = new int[] { 3 };
-
-            for (int i = 0; i < LoopCount; i++)
-            {
-                Parallel.For(0, src.Length, index => src[index] = (long)(double)Divider(src[index], divisor[0]));
-
-                int[] adder = new int[] { i };
-                Parallel.For(0, src.Length, index => src[index] = src[index] + adder[0]);
-            }
-
-            var output = matrix[new Slice(15, 25, 2), new Slice(15, 25, 2)];
-
-            sw.Stop();
-
-            Console.WriteLine(string.Format("Int64 calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
-            Console.WriteLine(output.ToString());
-            Console.WriteLine("************\n");
-        }
-
-        object Divider(dynamic src, dynamic divisor)
-        {
-            return (Int64)src / (double)divisor;
-        }
-
-        [Ignore]
-        [TestMethod]
-        public void Performance_MathOperation_Sin()
+        public void Performance_MathOperation_Sin_DOUBLE()
         {
 
             var a = np.arange(0, 10000000, dtype: np.Float64);
@@ -164,7 +126,7 @@ namespace NumpyDotNetTests
 
         [Ignore]
         [TestMethod]
-        public void Performance_AddReduce()
+        public void Performance_AddReduce_DOUBLE()
         {
 
             int LoopCount = 200;
@@ -189,7 +151,7 @@ namespace NumpyDotNetTests
 
         [Ignore]
         [TestMethod]
-        public void Performance_AddReduce2()
+        public void Performance_AddReduce2_DOUBLE()
         {
 
             int LoopCount = 200;
@@ -216,7 +178,7 @@ namespace NumpyDotNetTests
 
         [Ignore]
         [TestMethod]
-        public void Performance_AddAccumulate()
+        public void Performance_AddAccumulate_DOUBLE()
         {
 
             int LoopCount = 200;
@@ -239,7 +201,7 @@ namespace NumpyDotNetTests
 
         [Ignore]
         [TestMethod]
-        public void Performance_AddAccumulate2()
+        public void Performance_AddAccumulate2_DOUBLE()
         {
 
             int LoopCount = 200;
@@ -262,7 +224,7 @@ namespace NumpyDotNetTests
 
         [Ignore]
         [TestMethod]
-        public void Performance_AddReduce_IDEAL()
+        public void Performance_AddReduce_IDEAL_DOUBLE()
         {
 
             int LoopCount = 200;
@@ -307,7 +269,7 @@ namespace NumpyDotNetTests
 
         [Ignore]
         [TestMethod]
-        public void Performance_AddReduceAt()
+        public void Performance_AddReduceAt_DOUBLE()
         {
 
             int LoopCount = 200;
@@ -330,7 +292,7 @@ namespace NumpyDotNetTests
         }
         [Ignore]
         [TestMethod]
-        public void Performance_AddOuter()
+        public void Performance_AddOuter_DOUBLE()
         {
 
             int LoopCount = 200;
@@ -354,7 +316,7 @@ namespace NumpyDotNetTests
 
         [Ignore]
         [TestMethod]
-        public void Performance_AddOuter_NotSameType()
+        public void Performance_AddOuter_NotSameType_DOUBLE()
         {
 
             int LoopCount = 200;
@@ -376,9 +338,334 @@ namespace NumpyDotNetTests
             Console.WriteLine("************\n");
 
         }
+        #endregion
+
+        #region FLOAT tests
+        [Ignore]
+        [TestMethod]
+        public void Performance_ScalarOperation_FLOAT()
+        {
+            int LoopCount = 200;
+
+            var matrix = np.arange(1600000, dtype: np.Float32).reshape((40, -1));
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            //matrix = matrix["1:40:2", "1:-2:3"] as ndarray;
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                matrix = matrix / 3;
+                matrix = matrix + i;
+            }
+
+            var output = matrix[new Slice(15, 25, 2), new Slice(15, 25, 2)];
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("FLOAT calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine(output.ToString());
+            Console.WriteLine("************\n");
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_ScalarOperation_NotContiguous_FLOAT()
+        {
+            int LoopCount = 200;
+
+            var kk = new bool[Int32.MaxValue / 2];
+
+            var matrix = np.arange(16000000, dtype: np.Float32).reshape((40, -1));
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            matrix = matrix["1:40:2", "1:-2:3"] as ndarray;
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                matrix = matrix / 3;
+                matrix = matrix + i;
+            }
+
+            var output = matrix[new Slice(15, 25, 2), new Slice(15, 25, 2)];
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("FLOAT calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine(output.ToString());
+            Console.WriteLine("************\n");
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_ScalarOperation_InLine_FLOAT()
+        {
+            int LoopCount = 200;
+
+            // get the numeric ops background threads started
+            var x = np.arange(1);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+
+            var matrix = np.arange(1600000).astype(np.Float32).reshape((40, -1));
+
+            var src = matrix.Array.data.datap as double[];
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                Parallel.For(0, src.Length, index => src[index] = src[index] / 3);
+                Parallel.For(0, src.Length, index => src[index] = src[index] + i);
+            }
+
+            var output = matrix[new Slice(15, 25, 2), new Slice(15, 25, 2)];
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("FLOAT calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine(output.ToString());
+            Console.WriteLine("************\n");
+        }
+
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_MathOperation_Sin_FLOAT()
+        {
+
+            var a = np.arange(0, 10000000, dtype: np.Float32);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            var b = np.sin(a);
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("np.sin calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddReduce_FLOAT()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 10000000, dtype: np.Float32);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.reduce(UFuncOperation.add, a);
+                //Assert.AreEqual(49999995000000.0, b.item(0));
+                // print(b);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddReduce calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddReduce2_FLOAT()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 4000 * 10 * 4000, dtype: np.Float32).reshape(-1, 4000);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.reduce(UFuncOperation.add, a);
+                var c = np.sum(b);
+                Assert.AreEqual(1.279999992E+16, c.item(0));
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddReduce calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
 
 
 
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddAccumulate_FLOAT()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 10000000, dtype: np.Float32);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.accumulate(UFuncOperation.add, a);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddAccumulate calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddAccumulate2_FLOAT()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 4000 * 4000, dtype: np.Float32).reshape(4000, 4000);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.accumulate(UFuncOperation.add, a);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddAccumulate calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddReduce_IDEAL_FLOAT()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 10000000, dtype: np.Float32);
+            var aa = a.AsDoubleArray();
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                double total = 0;
+
+                long O2_CalculatedStep = 1;
+                long O2_CalculatedOffset = 0;
+                try
+                {
+                    for (int j = 0; j < aa.Length; j++)
+                    {
+
+                        long O2_Index = ((j * O2_CalculatedStep) + O2_CalculatedOffset);
+                        double O2Value = aa[O2_Index];                                            // get operand 2
+
+                        total = total + O2Value;
+                    }
+                    Assert.AreEqual(49999995000000.0, total);
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddReduce calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddReduceAt_FLOAT()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 10000000, dtype: np.Float32).reshape((40, -1));
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.reduceat(UFuncOperation.add, a, new long[] { 10, 20, 30, 39 });
+                print(b.shape);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddReduce calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddOuter_FLOAT()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 1000, dtype: np.Float32);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.outer(UFuncOperation.add, np.Float32, a, a);
+                // print(b.shape);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddReduce calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddOuter_NotSameType_FLOAT()
+        {
+
+            int LoopCount = 200;
+            var a1 = np.arange(0, 1000, dtype: np.Float32);
+            var a2 = np.arange(0, 1000, dtype: np.Int16);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.outer(UFuncOperation.add, np.Float32, a2, a1);
+                // print(b.shape);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddReduce calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+        #endregion
 
     }
 }
