e336b8d1b7288de7a4c3f6ea1f846793a391abd5
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2020-02-21 20:24:38 -0500

    adding COMPLEX performance tests

--- src/NumpyDotNet/UnitTests/NumpyDotNetTests/PerformanceTests.cs
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/PerformanceTests.cs
@@ -2309,5 +2309,332 @@ namespace NumpyDotNetTests
         }
         #endregion
 
+        #region COMPLEX tests
+        [Ignore]
+        [TestMethod]
+        public void Performance_ScalarOperation_COMPLEX()
+        {
+            int LoopCount = 200;
+
+            var matrix = np.arange(1600000, dtype: np.Complex).reshape((40, -1));
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            //matrix = matrix["1:40:2", "1:-2:3"] as ndarray;
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                matrix = matrix / 3;
+                matrix = matrix + i;
+            }
+
+            var output = matrix[new Slice(15, 25, 2), new Slice(15, 25, 2)];
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("DOUBLE calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine(output.ToString());
+            Console.WriteLine("************\n");
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_ScalarOperation_NotContiguous_COMPLEX()
+        {
+            int LoopCount = 200;
+
+            var kk = new bool[Int32.MaxValue / 2];
+
+            var matrix = np.arange(16000000, dtype: np.Complex).reshape((40, -1));
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            matrix = matrix["1:40:2", "1:-2:3"] as ndarray;
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                matrix = matrix / 3;
+                matrix = matrix + i;
+            }
+
+            var output = matrix[new Slice(15, 25, 2), new Slice(15, 25, 2)];
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("DOUBLE calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine(output.ToString());
+            Console.WriteLine("************\n");
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_ScalarOperation_InLine_COMPLEX()
+        {
+            int LoopCount = 200;
+
+            // get the numeric ops background threads started
+            var x = np.arange(1);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+
+            var matrix = np.arange(1600000).astype(np.Complex).reshape((40, -1));
+
+            var src = matrix.Array.data.datap as double[];
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                Parallel.For(0, src.Length, index => src[index] = src[index] / 3);
+                Parallel.For(0, src.Length, index => src[index] = src[index] + i);
+            }
+
+            var output = matrix[new Slice(15, 25, 2), new Slice(15, 25, 2)];
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("DOUBLE calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine(output.ToString());
+            Console.WriteLine("************\n");
+        }
+
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_MathOperation_Sin_COMPLEX()
+        {
+
+            var a = np.arange(0, 10000000, dtype: np.Complex);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            var b = np.sin(a);
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("np.sin calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddReduce_COMPLEX()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 10000000, dtype: np.Complex);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.reduce(UFuncOperation.add, a);
+                //Assert.AreEqual(49999995000000.0, b.item(0));
+                // print(b);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddReduce calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddReduce2_COMPLEX()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 4000 * 10 * 4000, dtype: np.Complex).reshape(-1, 4000);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.reduce(UFuncOperation.add, a);
+                var c = np.sum(b);
+                Assert.AreEqual(1.279999992E+16, c.item(0));
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddReduce calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+
+
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddAccumulate_COMPLEX()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 10000000, dtype: np.Complex);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.accumulate(UFuncOperation.add, a);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddAccumulate calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddAccumulate2_COMPLEX()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 4000 * 4000, dtype: np.Complex).reshape(4000, 4000);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.accumulate(UFuncOperation.add, a);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddAccumulate calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddReduce_IDEAL_COMPLEX()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 10000000, dtype: np.Complex);
+            var aa = a.AsDoubleArray();
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                double total = 0;
+
+                long O2_CalculatedStep = 1;
+                long O2_CalculatedOffset = 0;
+                try
+                {
+                    for (int j = 0; j < aa.Length; j++)
+                    {
+
+                        long O2_Index = ((j * O2_CalculatedStep) + O2_CalculatedOffset);
+                        double O2Value = aa[O2_Index];                                            // get operand 2
+
+                        total = total + O2Value;
+                    }
+                    Assert.AreEqual(49999995000000.0, total);
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddReduce calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddReduceAt_COMPLEX()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 10000000, dtype: np.Complex).reshape((40, -1));
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.reduceat(UFuncOperation.add, a, new long[] { 10, 20, 30, 39 });
+                print(b.shape);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddReduce calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddOuter_COMPLEX()
+        {
+
+            int LoopCount = 200;
+            var a = np.arange(0, 1000, dtype: np.Complex);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.outer(UFuncOperation.add, np.Complex, a, a);
+                // print(b.shape);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddReduce calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void Performance_AddOuter_NotSameType_COMPLEX()
+        {
+
+            int LoopCount = 200;
+            var a1 = np.arange(0, 1000, dtype: np.Complex);
+            var a2 = np.arange(0, 1000, dtype: np.Int16);
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < LoopCount; i++)
+            {
+                var b = np.ufunc.outer(UFuncOperation.add, np.Complex, a2, a1);
+                // print(b.shape);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(string.Format("AddReduce calculations took {0} milliseconds\n", sw.ElapsedMilliseconds));
+            Console.WriteLine("************\n");
+
+        }
+        #endregion
+
     }
 }
