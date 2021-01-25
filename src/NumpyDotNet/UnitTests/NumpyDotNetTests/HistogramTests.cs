c6eb8ed90188edd0c3830ebf60acdff602df4ceb
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2021-01-25 18:03:07 -0500

    add np.bincount unit tests.  First pass at getting histogram API running.

---
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/HistogramTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumpyDotNet;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NumpyLib;
+
+namespace NumpyDotNetTests
+{
+    [TestClass]
+    public class HistogramTests : TestBaseClass
+    {
+        [TestMethod]
+        public void test_bincount_1()
+        {
+            var x = np.arange(5);
+            var a = np.bincount(x);
+            print(a);
+
+            x = np.array(new int[] { 0, 1, 1, 3, 2, 1, 7 });
+            a = np.bincount(x);
+            print(a);
+
+            x = np.array(new int[] { 0, 1, 1, 3, 2, 1, 7, 23 });
+            a = np.bincount(x);
+            print(a);
+            print(a.size == (int)np.amax(x) + 1);
+
+        }
+
+        [TestMethod]
+        public void test_bincount_2()
+        {
+            var x = np.arange(5, dtype: np.Int64);
+            var a = np.bincount(x);
+            print(a);
+
+            x = np.array(new int[] { 0, 1, 1, 3, 2, 1, 7 }, dtype: np.Int16);
+            a = np.bincount(x);
+            print(a);
+
+            x = np.array(new int[] { 0, 1, 1, 3, 2, 1, 7, 23 }, dtype: np.Int8);
+            a = np.bincount(x);
+            print(a);
+            print(a.size == (int)np.amax(x) + 1);
+
+        }
+
+        [TestMethod]
+        public void test_bincount_3()
+        {
+
+            var w = np.array(new float[] { 0.3f, 0.5f, 0.2f, 0.7f, 1.0f, -0.6f });  // weights
+
+            var x = np.arange(5, dtype: np.Int64);
+            var a = np.bincount(x, weights: w);
+            print(a);
+
+            x = np.array(new int[] { 0, 1, 3, 2, 1, 7 }, dtype: np.Int16);
+            a = np.bincount(x, weights: w);
+            print(a);
+
+            x = np.array(new int[] { 0, 1, 3, 2, 1, 7 }, dtype: np.Int8);
+            a = np.bincount(x, weights: w);
+            print(a);
+            print(a.size == (int)np.amax(x) + 1);
+
+        }
+
+        [TestMethod]
+        public void test_bincount_4()
+        {
+            var x = np.arange(5, dtype: np.Int64);
+            var a = np.bincount(x, minlength: 8);
+            print(a);
+
+            x = np.array(new int[] { 0, 1, 1, 3, 2, 1, 7 }, dtype: np.Int16);
+            a = np.bincount(x, minlength: 10);
+            print(a);
+
+            x = np.array(new int[] { 0, 1, 1, 3, 2, 1, 7, 23 }, dtype: np.Int8);
+            a = np.bincount(x, minlength: 32);
+            print(a);
+            print(a.size == (int)np.amax(x) + 1);
+
+        }
+
+        [TestMethod]
+        public void test_bincount_uint64()
+        {
+            try
+            {
+                var x = np.arange(5, dtype: np.UInt64);
+                var a = np.bincount(x);
+                print(a);
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        [TestMethod]
+        public void test_bincount_double()
+        {
+            try
+            {
+                var x = np.arange(5, dtype: np.Float64);
+                var a = np.bincount(x);
+                print(a);
+            }
+            catch
+            {
+
+            }
+
+        }
+
+
+    }
+}
---
+++ src/PythonUnitTests/HistogramTests.py
@@ -0,0 +1,93 @@
+import unittest
+import numpy as np
+from nptest import nptest
+
+
+class HistogramTests(unittest.TestCase):
+
+
+    def test_bincount_1(self):
+
+        x = np.arange(5)
+        a = np.bincount(x)
+        print(a)
+
+        x = np.array([0, 1, 1, 3, 2, 1, 7])
+        a = np.bincount(x)
+        print(a)
+
+        x = np.array([0, 1, 1, 3, 2, 1, 7, 23])
+        a = np.bincount(x)
+        print(a)
+
+        print(a.size == np.amax(x)+1)
+
+    def test_bincount_2(self):
+
+        x = np.arange(5, dtype=np.int64)
+        a = np.bincount(x)
+        print(a)
+
+        x = np.array([0, 1, 1, 3, 2, 1, 7], dtype=np.int16)
+        a = np.bincount(x)
+        print(a)
+
+        x = np.array([0, 1, 1, 3, 2, 1, 7, 23], dtype=np.int8)
+        a = np.bincount(x)
+        print(a)
+
+        print(a.size == np.amax(x)+1)
+
+    def test_bincount_3(self):
+
+        w = np.array([0.3, 0.5, 0.2, 0.7, 1., -0.6]) # weights
+        x = np.arange(6, dtype=np.int64)
+        a = np.bincount(x, weights=w)
+        print(a)
+
+        x = np.array([0, 1, 3, 2, 1, 7], dtype=np.int16)
+        a = np.bincount(x,weights=w)
+        print(a)
+
+        x = np.array([0, 1, 3, 2, 1, 7], dtype=np.int8)
+        a = np.bincount(x, weights=w)
+        print(a)
+
+    def test_bincount_4(self):
+
+        x = np.arange(5, dtype=np.int64)
+        a = np.bincount(x, minlength=8)
+        print(a)
+
+        x = np.array([0, 1, 1, 3, 2, 1, 7], dtype=np.int16)
+        a = np.bincount(x, minlength=10)
+        print(a)
+
+        x = np.array([0, 1, 1, 3, 2, 1, 7, 23], dtype=np.int8)
+        a = np.bincount(x, minlength=32)
+        print(a)
+
+        print(a.size == np.amax(x)+1)
+
+    def test_bincount_uint64(self):
+
+        try :
+            x = np.arange(5, dtype=np.uint64)
+            a = np.bincount(x)
+            print(a)
+            self.fail("should have thrown exception")
+        except:
+            print("Exception occured")
+
+    def test_bincount_double(self):
+
+        try :
+            x = np.arange(5, dtype=np.float64)
+            a = np.bincount(x)
+            print(a)
+            self.fail("should have thrown exception")
+        except:
+            print("Exception occured")
+
+if __name__ == '__main__':
+    unittest.main()
