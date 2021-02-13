8c5ebcb73412f01c0857dc793b0181a7d76d8dba
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2021-02-13 14:08:05 -0500

    update version numbers

--- src/NumpyDotNet/UnitTests/NumpyDotNetTests/Properties/AssemblyInfo.cs
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NumpyDotNetTests")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
