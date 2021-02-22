3c067cf53f701fe62366ef40d3c63ec487e8b5b8
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2021-02-22 17:46:51 -0500

    prep unit tests for Financial functions

---
+++ src/PythonUnitTests/FinancialFunctionsTests.py
@@ -0,0 +1,11 @@
+import unittest
+import numpy as np
+import time as tm
+from nptest import nptest
+
+class FinancialFunctionsTests(unittest.TestCase):
+    def test_A(self):
+        self.fail("Not implemented")
+
+if __name__ == '__main__':
+    unittest.main()
