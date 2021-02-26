f53e9d90891c167ea247adfdcdbb4a361aa7f569
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2021-02-26 06:36:32 -0500

    python npf.pv unit tests

--- src/PythonUnitTests/FinancialFunctionsTests.py
+++ src/PythonUnitTests/FinancialFunctionsTests.py
@@ -278,6 +278,33 @@ class FinancialFunctionsTests(unittest.TestCase):
         res = npf.ppmt(0.1 / 12, np.arange(1,5), 24, 2000, 0,'begin')
         print(res)
 
+#region pv
+
+    def test_pv_DOUBLE(self):
+        res = npf.pv(0.07, 20, 12000)
+        print(res)
+
+        res = npf.pv(0.07, 20, 12000, 0)
+        print(res)
+
+        res = npf.pv(0.07, 20, 12000, 222220)
+        print(res)
+
+    def test_pv_begin_DOUBLE(self):
+        res = npf.pv(0.07, 20, 12000, 0, 1)
+        print(res)
+
+        res = npf.pv(0.07, 20, 12000, 0, 'begin')
+        print(res)
+
+    def test_pv_end_DOUBLE(self):
+        res = npf.pv(0.07, 20, 12000, 0, 0)
+        print(res)
+
+        res = npf.pv(0.07, 20, 12000, 0, 'end')
+        print(res)     
+#endregion
+
 #endregion
 if __name__ == '__main__':
     unittest.main()
--- src/PythonUnitTests/_financial.py
+++ src/PythonUnitTests/_financial.py
@@ -480,101 +480,101 @@ class npf(object):
         total = npf.pmt(rate, nper, pv, fv, when)
         return total - npf.ipmt(rate, per, nper, pv, fv, when)
 
+    @staticmethod
+    def pv(rate, nper, pmt, fv=0, when='end'):
+        """
+        Compute the present value.
 
-def pv(rate, nper, pmt, fv=0, when='end'):
-    """
-    Compute the present value.
+        Given:
+         * a future value, `fv`
+         * an interest `rate` compounded once per period, of which
+           there are
+         * `nper` total
+         * a (fixed) payment, `pmt`, paid either
+         * at the beginning (`when` = {'begin', 1}) or the end
+           (`when` = {'end', 0}) of each period
 
-    Given:
-     * a future value, `fv`
-     * an interest `rate` compounded once per period, of which
-       there are
-     * `nper` total
-     * a (fixed) payment, `pmt`, paid either
-     * at the beginning (`when` = {'begin', 1}) or the end
-       (`when` = {'end', 0}) of each period
+        Return:
+           the value now
 
-    Return:
-       the value now
+        Parameters
+        ----------
+        rate : array_like
+            Rate of interest (per period)
+        nper : array_like
+            Number of compounding periods
+        pmt : array_like
+            Payment
+        fv : array_like, optional
+            Future value
+        when : {{'begin', 1}, {'end', 0}}, {string, int}, optional
+            When payments are due ('begin' (1) or 'end' (0))
 
-    Parameters
-    ----------
-    rate : array_like
-        Rate of interest (per period)
-    nper : array_like
-        Number of compounding periods
-    pmt : array_like
-        Payment
-    fv : array_like, optional
-        Future value
-    when : {{'begin', 1}, {'end', 0}}, {string, int}, optional
-        When payments are due ('begin' (1) or 'end' (0))
+        Returns
+        -------
+        out : ndarray, float
+            Present value of a series of payments or investments.
 
-    Returns
-    -------
-    out : ndarray, float
-        Present value of a series of payments or investments.
+        Notes
+        -----
+        The present value is computed by solving the equation::
 
-    Notes
-    -----
-    The present value is computed by solving the equation::
+         fv +
+         pv*(1 + rate)**nper +
+         pmt*(1 + rate*when)/rate*((1 + rate)**nper - 1) = 0
 
-     fv +
-     pv*(1 + rate)**nper +
-     pmt*(1 + rate*when)/rate*((1 + rate)**nper - 1) = 0
+        or, when ``rate = 0``::
 
-    or, when ``rate = 0``::
+         fv + pv + pmt * nper = 0
 
-     fv + pv + pmt * nper = 0
+        for `pv`, which is then returned.
 
-    for `pv`, which is then returned.
+        References
+        ----------
+        .. [WRW] Wheeler, D. A., E. Rathke, and R. Weir (Eds.) (2009, May).
+           Open Document Format for Office Applications (OpenDocument)v1.2,
+           Part 2: Recalculated Formula (OpenFormula) Format - Annotated Version,
+           Pre-Draft 12. Organization for the Advancement of Structured Information
+           Standards (OASIS). Billerica, MA, USA. [ODT Document].
+           Available:
+           http://www.oasis-open.org/committees/documents.php?wg_abbrev=office-formula
+           OpenDocument-formula-20090508.odt
 
-    References
-    ----------
-    .. [WRW] Wheeler, D. A., E. Rathke, and R. Weir (Eds.) (2009, May).
-       Open Document Format for Office Applications (OpenDocument)v1.2,
-       Part 2: Recalculated Formula (OpenFormula) Format - Annotated Version,
-       Pre-Draft 12. Organization for the Advancement of Structured Information
-       Standards (OASIS). Billerica, MA, USA. [ODT Document].
-       Available:
-       http://www.oasis-open.org/committees/documents.php?wg_abbrev=office-formula
-       OpenDocument-formula-20090508.odt
+        Examples
+        --------
+        >>> import numpy as np
+        >>> import numpy_financial as npf
 
-    Examples
-    --------
-    >>> import numpy as np
-    >>> import numpy_financial as npf
+        What is the present value (e.g., the initial investment)
+        of an investment that needs to total $15692.93
+        after 10 years of saving $100 every month?  Assume the
+        interest rate is 5% (annually) compounded monthly.
 
-    What is the present value (e.g., the initial investment)
-    of an investment that needs to total $15692.93
-    after 10 years of saving $100 every month?  Assume the
-    interest rate is 5% (annually) compounded monthly.
+        >>> npf.pv(0.05/12, 10*12, -100, 15692.93)
+        -100.00067131625819
 
-    >>> npf.pv(0.05/12, 10*12, -100, 15692.93)
-    -100.00067131625819
+        By convention, the negative sign represents cash flow out
+        (i.e., money not available today).  Thus, to end up with
+        $15,692.93 in 10 years saving $100 a month at 5% annual
+        interest, one's initial deposit should also be $100.
 
-    By convention, the negative sign represents cash flow out
-    (i.e., money not available today).  Thus, to end up with
-    $15,692.93 in 10 years saving $100 a month at 5% annual
-    interest, one's initial deposit should also be $100.
+        If any input is array_like, ``pv`` returns an array of equal shape.
+        Let's compare different interest rates in the example above:
 
-    If any input is array_like, ``pv`` returns an array of equal shape.
-    Let's compare different interest rates in the example above:
+        >>> a = np.array((0.05, 0.04, 0.03))/12
+        >>> npf.pv(a, 10*12, -100, 15692.93)
+        array([ -100.00067132,  -649.26771385, -1273.78633713]) # may vary
 
-    >>> a = np.array((0.05, 0.04, 0.03))/12
-    >>> npf.pv(a, 10*12, -100, 15692.93)
-    array([ -100.00067132,  -649.26771385, -1273.78633713]) # may vary
+        So, to end up with the same $15692.93 under the same $100 per month
+        "savings plan," for annual interest rates of 4% and 3%, one would
+        need initial investments of $649.27 and $1273.79, respectively.
 
-    So, to end up with the same $15692.93 under the same $100 per month
-    "savings plan," for annual interest rates of 4% and 3%, one would
-    need initial investments of $649.27 and $1273.79, respectively.
-
-    """
-    when = _convert_when(when)
-    (rate, nper, pmt, fv, when) = map(np.asarray, [rate, nper, pmt, fv, when])
-    temp = (1+rate)**nper
-    fact = np.where(rate == 0, nper, (1+rate*when)*(temp-1)/rate)
-    return -(fv + pmt*fact)/temp
+        """
+        when = npf._convert_when(when)
+        (rate, nper, pmt, fv, when) = map(np.asarray, [rate, nper, pmt, fv, when])
+        temp = (1+rate)**nper
+        fact = np.where(rate == 0, nper, (1+rate*when)*(temp-1)/rate)
+        return -(fv + pmt*fact)/temp
 
 # Computed with Sage
 #  (y + (r + 1)^n*x + p*((r + 1)^n - 1)*(r*w + 1)/r)/(n*(r + 1)^(n - 1)*x -
