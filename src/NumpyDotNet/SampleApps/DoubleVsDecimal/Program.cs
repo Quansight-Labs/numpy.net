9fa65ecd4eae4f7213378b9323bef635f4f156bc
KevinBaselinesw <KevinBaselinesw@users.noreply.github.com>
2021-03-10 06:04:24 -0500

    update numpydotnet package versions

--- src/NumpyDotNet/SampleApps/DoubleVsDecimal/Program.cs
+++ src/NumpyDotNet/SampleApps/DoubleVsDecimal/Program.cs
@@ -1,5 +1,4 @@
 ﻿using NumpyDotNet;
-using NumpyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- src/NumpyDotNet/SampleApps/WpfApp1/MainWindow.xaml.cs
+++ src/NumpyDotNet/SampleApps/WpfApp1/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using NumpyDotNet;
-using NumpyLib;
+
 
 
 namespace WPFApp1
