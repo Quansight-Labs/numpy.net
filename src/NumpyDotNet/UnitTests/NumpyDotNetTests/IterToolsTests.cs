1966972565a5f966843810393b1e4e80b188055f
KevinBaselinesw <kmckenna@baselinesw.com>
2022-10-23 08:02:29 -0400

    add some unit tests to prove out IterTools.product

---
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/IterToolsTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumpyDotNet;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NumpyLib;
+#if NPY_INTP_64
+using npy_intp = System.Int64;
+#else
+using npy_intp = System.Int32;
+#endif
+
+namespace NumpyDotNetTests
+{
+    [TestClass]
+    public class IterToolsTests  
+    {
+        [TestMethod]
+        public void IterTools_Product_1()
+        {
+            List<object> products = new List<object>();
+
+            foreach (var iter in IterTools.products(new string[] { "ABC", "xy", "12" }, 1))
+            {
+                products.Add(iter);
+            }
+
+            products.Clear();
+            foreach (var iter in IterTools.products(new object[] { 0, 1 }, 3))
+            {
+                products.Add(iter);
+            }
+
+        }
+    }
+}
