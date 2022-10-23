04b59aa1511e9317497b3385151e95171e33b372
KevinBaselinesw <kmckenna@baselinesw.com>
2022-10-23 08:31:29 -0400

    move IterTools code to it's own module

--- src/PythonUnitTests/nptest.py
+++ src/PythonUnitTests/nptest.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import numpy.core.numeric as _nx
 from numpy.core import linspace, atleast_1d, atleast_2d, transpose
+from IterTools import IterTools
 #from numpy.core.numeric import (
 #    ones, zeros, arange, concatenate, array, asarray, asanyarray, empty,
 #    empty_like, ndarray, around, floor, ceil, take, dot, where, intp, multiply,
@@ -1341,76 +1342,13 @@ class nptest(object):
                                  (slice(-offset, None), slice(None, offset)))
 
             result = np.empty_like(a)
-            #for indices in itertools.product(*rolls):
-            for indices in nptest.product(*rolls):
+            for indices in IterTools.product(*rolls):
                 arr_index, res_index = zip(*indices)
                 result[res_index] = a[arr_index]
 
             return result
 
-    @staticmethod
-    def product(*args):
-        repeat = 1
-        # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
-        # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
-        pools = [tuple(pool) for pool in args] * repeat
-        result = [[]]
-        for pool in pools:
-            result = [x+[y] for x in result for y in pool]
-        for prod in result:
-            yield tuple(prod)
-
-    @staticmethod
-    def cartesian(arrays, out=None):
-        """
-        Generate a cartesian product of input arrays.
-
-        Parameters
-        ----------
-        arrays : list of array-like
-            1-D arrays to form the cartesian product of.
-        out : ndarray
-            Array to place the cartesian product in.
-        
-        Returns
-        -------
-        out : ndarray
-            2-D array of shape (M, len(arrays)) containing cartesian products
-            formed of input arrays.
-
-        Examples
-        --------
-        >>> cartesian(([1, 2, 3], [4, 5], [6, 7]))
-        array([[1, 4, 6],
-               [1, 4, 7],
-               [1, 5, 6],
-               [1, 5, 7],
-               [2, 4, 6],
-               [2, 4, 7],
-               [2, 5, 6],
-               [2, 5, 7],
-               [3, 4, 6],
-               [3, 4, 7],
-               [3, 5, 6],
-               [3, 5, 7]])
-
-        """
-
-        arrays = [np.asarray(x) for x in arrays]
-        dtype = arrays[0].dtype
-
-        n = np.prod([x.size for x in arrays])
-        if out is None:
-            out = np.zeros([n, len(arrays)], dtype=dtype)
-
-        m = n / arrays[0].size
-        out[:,0] = np.repeat(arrays[0], m)
-        if arrays[1:]:
-            cartesian(arrays[1:], out=out[0:m,1:])
-            for j in xrange(1, arrays[0].size):
-                out[j*m:(j+1)*m,1:] = out[0:m,1:]
-        return out
-    
+   
     @staticmethod
     def rollaxis(a, axis, start=0):
 
