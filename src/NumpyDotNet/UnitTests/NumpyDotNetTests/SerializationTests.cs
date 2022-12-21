5b6eb50d20b378bfdf4f79f4650af36268b9c41c
KevinBaselinesw <kmckenna@baselinesw.com>
2022-12-21 09:29:14 -0500

    adding SerializationTests to unit test suite

---
+++ src/NumpyDotNet/UnitTests/NumpyDotNetTests/SerializationTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumpyDotNet;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NumpyLib;
+using Newtonsoft.Json;
+#if NPY_INTP_64
+using npy_intp = System.Int64;
+#else
+using npy_intp = System.Int32;
+#endif
+
+namespace NumpyDotNetTests
+{
+    [TestClass]
+    public class SerializationTests : TestBaseClass
+    {
+  
+
+        [TestMethod]
+        public void test_shape_serialization_newtonsoft()
+        {
+            var a = np.arange(9).reshape(3, 3);
+            print(a);
+            AssertShape(a, 3, 3);
+
+            var ashapeSerialized = JsonConvert.SerializeObject(a.shape);
+
+            var b = a.reshape(JsonConvert.DeserializeObject<shape>(ashapeSerialized));
+            print(b);
+
+        }
+
+
+    }
+}
